f3b07adac7d4bf503a65194c4910a2762e9d357e
J. Cohen <hostelmarketing@gmail.com>
2013-11-04 21:20:09 -0800

    Page 15

--- assets/js/app.js
+++ assets/js/app.js
@@ -90,7 +90,64 @@ function shuffleDeck(o) {
 }
 
 function dealCard() {
+    var shoeArray = [],
+        shoeArrayStringified,
+        cardFace,
+        cardSuit,
+        cardImage,
+        cardRegEx,
+        cardFaces,
+        cardFileFaces,
+        cardFaceIndex,
+        cardFileFace,
+        cardValues;
 
+    // Get the deck(s) out of localStorage in string form
+    shoeArrayStringified = localStorage.getItem('shoe');
+
+    // JSONify it
+    shoeArray = JSON.parse(shoeArrayStringified);
+
+    // Get the first item
+    card = shoeArray[0].splice(0, 1);
+
+    // Make card an object instead of a one-element array
+    card = card[0];
+
+    cardFace = card.face;
+    cardSuit = card.suit;
+
+    // If card is a single-digit number, pad a zero for filename
+    //cardRegEx = cardFace.match(/^[0-9]$/);
+    //if (cardRegEx !== null) {
+        //cardFace = '0'+ cardFace;
+    //}
+    
+    // Prepare to substitute filename values for the card faces
+    cardFaces = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+    cardFileFaces = ['01','02','03','04','05','06','07','08','09','10','11','12','13'];
+    cardValues = [1,2,3,4,5,6,7,8,9,10,10,10,10];
+    
+    cardFaceIndex = cardFaces.indexOf(cardFace);
+    cardFileFace = cardFileFaces[cardFaceIndex];
+
+    // Example file name: h02.png
+    cardImage = cardSuit.toLowerCase() + cardFace + '.png';
+    log(cardImage);
+
+
+//• cardValue
+//• This should be an integer value.
+//• You will need to apply suitable logic to derive the value:
+//• All cards with a numeric ‘face’ property will have a value that is the same as the
+//property’s value.
+//• Jacks, Queens and Kings all have a value of 10.
+//• Aces have a value of 1.
+//• Next, save the shoe array (now with one element removed) back to local storage.
+//• Finally, the function should create and return an object:
+//• The object should have two properties with keys/values as follows:
+//• image: the image file name derived from the card (e.g. ‘h12.jpg’).
+//• value: the value derived from the card (e.g. 10
 }
 
 function dealHands() {
